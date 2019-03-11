df882a758ec78844a6d2d74be015ab9345827a40
unknown <solah0980@gmail.com>
2019-03-11 07:34:29 +0700

    'project1'

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,25 @@
-console.log('Hello')
+let express = require('express')
+const app = express()
+
+app.get('/status', function(req, res){
+    res.send('Hello World')
+})
+
+let port = 8081
+
+app.listen(port, function(){
+    console.log('server on' + port)
+})
+
+app.get('/hello/:person', function(req, req){
+    console.log('hello - ' + req.params.person)
+    app.send('say hello with' + req.params.person)
+})
+
+app.get('/user/:userId', function(req, res){
+    app.send('ดูข้อมูลผู้ใช้')
+})
+
+app.get('/users', function(req, res){
+    app.send('เรียกข้อมูล ผู้ใช้ทั้งหมด')
+})
